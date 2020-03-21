9d35e05b5c88f2dddebdb77323fef0380d1b2a58
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-03-21 16:10:55 +0300

    init:" start towel-soft task"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    if(!Array.isArray(matrix)){return []}
+  var recal = [];
+  var dirORrev = true;
+  for(var i = 0 ; i < matrix.length ; i++){
+    if(dirORrev){
+      for(var k = 0; k < matrix[i].length ; k++){
+        recal.push(matrix[i][k])
+      }
+      dirORrev = false;
+    }
+    else{for(var k = matrix[i].length - 1; k >= 0; k--){
+      recal.push(matrix[i][k]);
+       }
+       dirORrev = true;
+    }
+    
+  }
+  return recal;
 }
